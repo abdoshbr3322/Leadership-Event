864878a7372a8e789b5832f761f7184a8b947b3b
abdoshbr3322 <abdullahelshebrawy@gmail.com>
2022-08-06 22:40:18 +0200

    Finished About

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ this template is named Leadership Event and i made to improve my skills
 
 
 Live Demo: <a href="https://abdoshbr3322.github.io/Leadership-Event/">https://abdoshbr3322.github.io/Leadership-Event/</a>
+
+<a href="https://storage.googleapis.com/drive-bulk-export-anonymous/20220801T191316Z/4133399871716478688/b132c8c5-9539-40b5-9925-899ccbccd7ee/1/7603df2c-f66b-45fb-bbe1-619df0e3fa21?authuser">Link</a>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-  $(".nav-toggle").click(function () {
-    $(this).toggleClass("nav-active")
-  })
-})
+  $(".nav-toggle").click(toggleNav);
+  $(".day").click(selectDay);
+});
+
+function toggleNav() {
+  $(this).toggleClass("nav-active");
+  $(".mobile-nav").slideToggle();
+}
+
+function selectDay() {
+  let clicked = $(this);
+  $(".day").removeClass("active");
+  clicked.addClass("active");
+  $(".shedule").fadeOut()
+  $(".shedule").filter(function (_ind ,_el) {
+    return _el.attr("data-day") === clicked.attr("data-day");
+  }).fadeIn()
+}
