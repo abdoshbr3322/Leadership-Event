b039056db1c2daea47e025a58746601b199e19fa
abdoshbr3322 <abdullahelshebrawy@gmail.com>
2022-08-11 10:00:28 +0200

    Finished The Whole Project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $(".nav-toggle").click(toggleNav);
   $(".day").click(selectDay);
+  $(window).scroll(checkPosition);
+  $(".scroll-top").click(function () {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    })
+  })
 });
 
 function toggleNav() {
@@ -14,4 +21,14 @@ function selectDay() {
   $clicked.addClass("active");
   $(`.shedules .event:not(${$clicked.attr("data-day")})`).fadeOut();
   $($clicked.attr("data-day")).fadeIn();
+}
+
+function checkPosition() {
+  let sectionsLinks = $("header .nav li a");
+  sectionsLinks.removeClass("active")
+  let currentSectionLink = sectionsLinks.filter(function (_ind, _el) {
+    let section = $($(_el).attr("href"));
+    return (section.offset().top + section.height()) >= $(window).scrollTop();
+  })[0];
+  $(currentSectionLink).addClass("active");
 }
